1ffbdc40bff5cfdb64e5d450522240f4a951e7b8
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-10-24 17:19:16 -0700

    Profile Pic added
    Profile Pic added as a component with basic css

---
+++ vite-project/src/components/ProfilePic.tsx
@@ -0,0 +1,12 @@
+export function ProfilePic() {
+    const baseUrl = import.meta.env.BASE_URL;
+    const imagePath = "src/assets/images/Jaafar.jpg";
+
+    return (
+        <img
+            src={`${baseUrl}${imagePath}`}
+            alt = "Profile Picture of Jaafar Rodgers"
+            className="w-1/2 rounded-full ml-5 mt-3" 
+        />
+    )
+}
--- vite-project/src/pages/AboutMePage.tsx
+++ vite-project/src/pages/AboutMePage.tsx
@@ -1,9 +1,11 @@
 import '../assets/css/App.css';
+import { ProfilePic } from '../components/ProfilePic';
 
 const AboutMePage = () => {
     return (
         <div>
             <h1>About Me Page</h1>
+            <ProfilePic />
         </div>
     );
 };
