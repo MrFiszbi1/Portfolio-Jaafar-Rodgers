59cac7f6e45c053b79660199ef07d9a5ed0c4ea3
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-11-02 17:46:57 -0700

    Update Bio.tsx
    Bio tag fixed

--- vite-project/src/components/Bio.tsx
+++ vite-project/src/components/Bio.tsx
@@ -1,12 +1,10 @@
 export function Bio() {
     return (
-        <div className="bg-black rounded overflow-auto w-3/4 md:w-1/2 h-full mx-3">
-            <p className="mx-3 mt-3">
-                I am a dedicated and results-driven professional with a solid foundation in Computer Science,
-                backed by a Bachelor of Science in Computer Science (GPA: 3.62) from Portland State University (PSU).
-                My primary focus is on software development and IT solutions. My passion lies in leveraging my creativity to craft innovative solutions
-                and visualize the digital world through graphics, animations, and websites.
-            </p>
-        </div>
+        <p className="bg-black rounded overflow-auto w-3/4 md:w-1/2 h-fit mx-3 px-3 pt-4 pb-6">
+            I am a dedicated and results-driven professional with a solid foundation in Computer Science,
+            backed by a Bachelor of Science in Computer Science (GPA: 3.62) from Portland State University (PSU).
+            My primary focus is on software development and IT solutions. My passion lies in leveraging my creativity to craft innovative solutions
+            and visualize the digital world through graphics, animations, and websites.
+        </p>
     )
 }
