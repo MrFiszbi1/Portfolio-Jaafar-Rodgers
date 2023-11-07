9f956022d3f0b94dbdc3df2ca98c924e7bc0ee87
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-11-06 19:01:41 -0800

    LinkedIn button added
    LinkedIn button added CSS not fixed

--- vite-project/src/components/Bio.tsx
+++ vite-project/src/components/Bio.tsx
@@ -1,10 +1,15 @@
+import { INButton } from '../components/INButton';
+
 export function Bio() {
     return (
-        <p className="bg-black rounded overflow-auto w-3/4 md:w-1/2 h-fit mx-3 px-3 pt-4 pb-6">
-            I am a dedicated and results-driven professional with a solid foundation in Computer Science,
-            backed by a Bachelor of Science in Computer Science (GPA: 3.62) from Portland State University (PSU).
-            My primary focus is on software development and IT solutions. My passion lies in leveraging my creativity to craft innovative solutions
-            and visualize the digital world through graphics, animations, and websites.
-        </p>
+        <div className="bg-black rounded overflow-auto w-3/4 md:w-1/2 h-fit mx-3 px-3 pt-4 pb-6">
+            <p>
+                I am a dedicated and results-driven professional with a solid foundation in Computer Science,
+                backed by a Bachelor of Science in Computer Science (GPA: 3.62) from Portland State University (PSU).
+                My primary focus is on software development and IT solutions. My passion lies in leveraging my creativity to craft innovative solutions
+                and visualize the digital world through graphics, animations, and websites.
+            </p>
+            <INButton />
+        </div>
     )
 }
---
+++ vite-project/src/components/INButton.tsx
@@ -0,0 +1,15 @@
+export function INButton() {
+    return (
+        <a href="https://www.linkedin.com/in/jaafar-rodgers/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">
+            <img
+            src="/linkedin.svg" 
+            alt="Link to LinkedIn page"
+            width="50"
+            height="50"
+            />
+        </a>
+    );
+};
