d337981dfd5f2beae9d6a8b9f777c9f8b1b855ac
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-11-14 16:04:29 -0800

    tab logo added
    Tab logo added

--- vite-project/src/components/ProfilePic.tsx
+++ vite-project/src/components/ProfilePic.tsx
@@ -8,7 +8,7 @@ export function ProfilePic() {
             alt = "Profile Picture of Jaafar Rodgers"
             width = "200"
             height = "200"
-            className="rounded-full mt-6 mx-6" 
+            className="border-black border-4 rounded-full mt-6 mx-6 shadow-2xl" 
         />
     )
 }
