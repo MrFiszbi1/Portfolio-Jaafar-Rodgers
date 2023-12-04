480ee30b8ef3f7d25a49ec7ed7c52a4228145b25
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-12-03 21:28:47 -0800

    Education added
    Education added based off resume

--- vite-project/src/pages/ResumePage.tsx
+++ vite-project/src/pages/ResumePage.tsx
@@ -1,9 +1,11 @@
 import '../assets/css/PortfolioStyles.css';
 import { Experience } from '../components/Experience';
+import { Education } from '../components/Education';
 
 const ResumePage = () => {
     return (
         <div>
+            <Education />
             <Experience />
         </div>
     );
