505e8edbd251de08a5dec24ec7ad5f2769b46bcb
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2024-01-12 18:40:14 -0800

    CSS text changes
    CSS text changes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,7 +4,7 @@ export function Education() {
             <h1 className="resumeSectionTitle">Education</h1>
             <div className="resumeEducationContent">
                 <span>Bachelor of Science in Computer Science (2023)</span>
-                <span>Portland State University - GPA: 3.7</span>
+                <span className="jobDate">Portland State University - GPA: 3.7</span>
             </div>
         </div>
     )
