61531b57407310cf2de061eb7731224e836adc22
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2024-01-17 21:28:46 -0800

    Education updated
    AWS Cert added

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -6,6 +6,11 @@ export function Education() {
                 <span>Bachelor of Science in Computer Science (2023)</span>
                 <span className="jobDate">Portland State University - GPA: 3.7</span>
             </div>
+
+            <div className="resumeEducationContent">
+                <span>AWS Certified Cloud Practitioner</span>
+                <span className="jobDate">Jan 2024</span>
+            </div>
         </div>
     )
 }
